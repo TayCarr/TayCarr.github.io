397105618f86bc3315890e26ca8f9916526233a4
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-07-31 19:20:12 -0600

    base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //base: '/TayCarr.github.io/'
-  //base: '/'
-  base: '/TayCarr/'
+  base: '/'
+  //base: '/TayCarr/'
 })
