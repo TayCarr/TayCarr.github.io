7078cb5518fd84598e7bc880c6200894ee359b91
TayCarr <117793653+TayCarr@users.noreply.github.com>
2026-02-23 15:05:19 -0700

    more css added media query for laptop screens

--- src/components/BookModal.jsx
+++ src/components/BookModal.jsx
@@ -1,14 +1,15 @@
 function BookModal({ book, close }) {
+    console.log(book.coverIMG)
     return (
       <div className="modal-overlay" onClick={close}>
         <div className="modal" onClick={(e) => e.stopPropagation()}>
-          <h2>{book.title}</h2>
-          {/*<img src={book.coverIMG} className="book-cover" />*/}
-          <p><strong>Author:</strong> {book.author}</p>
-          <p><strong>Status:</strong> {book.status}</p>
-          <p><strong>Repo Folder:</strong> {book.repoFolder}</p>
+          <h2 className="book-details">{book.title}</h2>
+          <img src={book.coverIMG} className="book-cover" />
+          <p className="book-details"><strong>Author:</strong> {book.author}</p>
+          <p className="book-details"><strong>Status:</strong> {book.status}</p>
+          <p className="book-details"><strong>Repo Folder:</strong> {book.repoFolder}</p>
           
-          <button onClick={close}>Close</button>
+          <button className="details-button" onClick={close}>Close</button>
         </div>
       </div>
     );
